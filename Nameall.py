0e33d29c4c73ea8ba3dfe983e6cd6fb3a67c7076
MaximeParizot <104005385+MaximeParizot@users.noreply.github.com>
2024-05-15 10:08:17 +0200

    Add files via upload

--- Nameall.py
+++ Nameall.py
@@ -2,10 +2,12 @@ import os
 
 filelist=os.listdir('OrthologyRelationships')
 d={}
-d['1ToMany']=filelist[0]
-d['1To1']=filelist[2]
-d['manToMany']=filelist[3]
+d['1ToMany']='rootedTre_orthology1ToMany.txt'
+d['1To1']='rootedTre_orthology1To1.txt'
+d['manyToMany']='rootedTre_orthologymanyToMany.txt'
 
 for file in d.keys(): 
-    commande='python3 Nametunicates.py -f OrthologyRelationships/'+d[file]+' -out Outputs/'+file+' -lim 100 -check n'
+    commande='python3 Nametunicates_hier.py -f OrthologyRelationships/'+d[file]+' -out Outputs/'+file+' -check n'
+    os.system(commande)
+    commande='python3 Nametunicates_all.py -f OrthologyRelationships/'+d[file]+' -out Outputs/'+file+' -check n'
     os.system(commande)
